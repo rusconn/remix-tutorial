92e44e8b6ea12df97d1fb4b99ed7a77b73000cd2
rusconn <rusconn3@gmail.com>
2023-10-28 20:18:25 +0900

    feat(css): global pending ui

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 
 import { createEmptyContact, getContacts } from "./data";
@@ -32,6 +33,7 @@ export const action = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
 
   return (
     <html lang="en">
@@ -89,7 +91,10 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div
+          className={navigation.state === "loading" ? "loading" : ""}
+          id="detail"
+        >
           <Outlet />
         </div>
         <ScrollRestoration />
