2cb5c472976ecd2035a8dbf821891a1096c1c3d6
rusconn <rusconn3@gmail.com>
2023-10-29 00:55:22 +0900

    chore: esmodules

--- remix.config.mjs
+++ remix.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
+export default {
   ignoredRouteFiles: ["**/.*"],
-  serverModuleFormat: "cjs",
+  serverModuleFormat: "esm",
 };
